e7330fcc022cd45a61c11814bd6c3a02661aad0f
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-24 20:17:05 -0500

    Add initial files and psuedo code

---
+++ script.js
@@ -0,0 +1,15 @@
+//Factory function for creating players and assigning symbols
+
+//Function for changing turns and assigning first player
+
+//Function for placing symbols onto the board 
+
+//Function for displaying symbols on a board
+
+//Function to keep track of the board
+
+//Function for checking if there is three in a row
+
+//Function for reseting the board
+
+//Function for new game 
