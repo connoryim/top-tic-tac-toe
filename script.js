37e2e5bed343d1df74455faa0aa46b02a7a7cef0
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-30 16:15:23 -0500

    Style new game page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Factory function for creating players and assigning symbols
+
 function newPlayer (name){
     return{
         name : name,
@@ -9,9 +10,73 @@ function newPlayer (name){
     };
 };
 
-const playerOne = newPlayer("Tyler");
-console.log(playerOne);
-playerOne.playerSymbol("a")
+function getPlayers (){
+    const divPlayer = document.createElement("div");
+    const container = document.getElementById("container");
+    
+    const inputPlayer = document.createElement("input");
+    inputPlayer.setAttribute("type","text")
+    
+    //Info input for first player
+    const blockPlayerOne = document.createElement("div");
+    blockPlayerOne.setAttribute("id","blockPlayerOne");
+    
+    const firstInputPlayer = inputPlayer.cloneNode(true);
+    firstInputPlayer.setAttribute("id","firstInputPlayer");
+    
+    const textPlayerOne = document.createElement("span");
+    textPlayerOne.textContent = "Player 1:";
+
+    blockPlayerOne.appendChild(textPlayerOne);
+    blockPlayerOne.appendChild(firstInputPlayer);
+
+    //Info input for second player
+    const blockPlayerTwo = document.createElement("div");
+    blockPlayerTwo.setAttribute("id","blockPlayerTwo");
+    
+    const secondInputPlayer = inputPlayer.cloneNode(true);
+    secondInputPlayer.setAttribute("id","secondInputPlayer");
+
+    const textPlayerTwo = document.createElement("span");
+    textPlayerTwo.textContent = "Player 2:";
+
+    blockPlayerTwo.appendChild(textPlayerTwo);
+    blockPlayerTwo.appendChild(secondInputPlayer);
+
+
+    //Block for buttons
+    const buttonsBlock = document.createElement("div");
+    buttonsBlock.setAttribute("id","buttonsBlock");
+    
+    const symbolButtonOne = document.createElement("button");
+    symbolButtonOne.classList.add("x");
+    symbolButtonOne.textContent = "X";
+
+    const symbolButtonTwo = document.createElement("button");
+    symbolButtonTwo.classList.add("o")
+    symbolButtonTwo.textContent = "0";
+
+    const newGameButton = document.createElement("button");
+    newGameButton.setAttribute("id","newGameButton")
+    newGameButton.textContent = "New Game";
+
+    
+    buttonsBlock.appendChild(symbolButtonOne);
+    buttonsBlock.appendChild(symbolButtonTwo);
+    
+
+    divPlayer.setAttribute("id","getPlayers");
+    divPlayer.appendChild(blockPlayerOne);
+    divPlayer.appendChild(buttonsBlock);
+    divPlayer.appendChild(blockPlayerTwo);
+    divPlayer.appendChild(newGameButton);
+    container.appendChild(divPlayer);
+
+};
+
+getPlayers();
+
+//Function for assigning symbol to player
 
 //Function for changing turns and assigning first player
 
