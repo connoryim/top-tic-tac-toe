7763c7b4c56382050875859a9f78a4d58fffb780
connoryim <0134269333+connoryim@users.noreply.github.com>
2024-01-30 23:05:21 -0500

    New game creates board of buttons

--- script.js
+++ script.js
@@ -49,16 +49,21 @@ function getPlayers (){
     buttonsBlock.setAttribute("id","buttonsBlock");
     
     const symbolButtonOne = document.createElement("button");
+    symbolButtonOne.setAttribute("id","symbolButtonOne");
     symbolButtonOne.classList.add("x");
     symbolButtonOne.textContent = "X";
+    symbolButtonOne.addEventListener("click",switchSymbols);
 
     const symbolButtonTwo = document.createElement("button");
-    symbolButtonTwo.classList.add("o")
-    symbolButtonTwo.textContent = "0";
+    symbolButtonTwo.setAttribute("id","symbolButtonTwo");
+    symbolButtonTwo.classList.add("o");
+    symbolButtonTwo.textContent = "O";
+    symbolButtonTwo.addEventListener("click",switchSymbols);
 
     const newGameButton = document.createElement("button");
     newGameButton.setAttribute("id","newGameButton")
     newGameButton.textContent = "New Game";
+    newGameButton.addEventListener("click", setupGame);
 
     
     buttonsBlock.appendChild(symbolButtonOne);
@@ -73,6 +78,144 @@ function getPlayers (){
     container.appendChild(divPlayer);
 
 };
+
+
+
+function switchSymbols(){
+    console.log(this);
+    let otherButton = ""
+    if(this.matches("#symbolButtonOne")){
+        otherButton = document.getElementById("symbolButtonTwo")    
+        if(this.classList.contains("x")){
+            this.classList.add("o");
+            this.classList.remove("x");
+            this.textContent = "O"
+            otherButton.classList.remove("o")
+            otherButton.classList.add("x")
+            otherButton.textContent = "X"
+        }else if(this.classList.contains("o")){
+            this.classList.add("x");
+            this.classList.remove("o");
+            this.textContent = "X"
+            otherButton.classList.remove("x")
+            otherButton.classList.add("o")
+            otherButton.textContent = "O"
+        }
+    }else if(this.matches("#symbolButtonTwo")){
+        otherButton = document.getElementById("symbolButtonOne")
+        if(this.classList.contains("x")){
+            this.classList.add("o");
+            this.classList.remove("x");
+            this.textContent = "O"
+            otherButton.classList.remove("o")
+            otherButton.classList.add("x")
+            otherButton.textContent = "X"
+        }else if(this.classList.contains("o")){
+            this.classList.add("x");
+            this.classList.remove("o");
+            this.textContent = "X"
+            otherButton.classList.remove("x")
+            otherButton.classList.add("o")
+            otherButton.textContent = "O"
+        }
+    }
+};
+
+function setupGame(){
+    this.parentNode.remove()
+
+    const boardSymbols = [
+        {
+            a1:"",
+            a2:"",
+            a3:""
+        },
+        {
+            b1:"",
+            b2:"",
+            b3:""
+        },
+        {
+            c1:"",
+            c2:"",
+            c3:"",
+        }
+    ]
+
+
+    const gameContainer = document.createElement("div");
+    gameContainer.setAttribute("id","gameContainer");
+
+    const newPlayers = document.createElement("button");
+    newPlayers.textContent = "New Players";
+    newPlayers.addEventListener("click", ()=>{       
+        getPlayers();
+        gameContainer.remove();
+    });
+    newPlayers.setAttribute("id","newPlayers")
+
+    const scoreBoard = document.createElement("div");
+    scoreBoard.setAttribute("id","scoreBoard");
+
+
+    //Score display for player one
+    const scorePlayerOne = document.createElement("div");
+    scorePlayerOne.setAttribute("id","scorePlayerOne");
+    
+    const namePlayerOne = document.createElement("span");
+    namePlayerOne.setAttribute("id","namePlayerOne");
+    namePlayerOne.textContent = "player 1: "
+    
+    const pointsPlayerOne = document.createElement("span");
+    pointsPlayerOne.setAttribute("id","pointsPlayerOne");
+    pointsPlayerOne.textContent = 0;
+
+    scorePlayerOne.appendChild(namePlayerOne);
+    scorePlayerOne.appendChild(pointsPlayerOne);
+
+    scoreBoard.appendChild(scorePlayerOne);
+    
+    //Score display for player two
+    const scorePlayerTwo = document.createElement("div");
+    scorePlayerTwo.setAttribute("id","scorePlayerTwo");
+    
+    const namePlayerTwo = document.createElement("span");
+    namePlayerTwo.setAttribute("id","namePlayerTwo");
+    namePlayerTwo.textContent = "player 2: "
+    
+    const pointsPlayerTwo = document.createElement("span");
+    pointsPlayerTwo.setAttribute("id","pointsPlayerTwo");
+    pointsPlayerTwo.textContent = 0;
+
+    scorePlayerTwo.appendChild(namePlayerTwo);
+    scorePlayerTwo.appendChild(pointsPlayerTwo);
+
+    scoreBoard.appendChild(scorePlayerTwo);
+
+
+    const gameBoard = document.createElement("div");
+    gameBoard.setAttribute("id","gameBoard")
+
+    
+    for(i=0; i<boardSymbols.length; i++){
+        for(var nameSpace in boardSymbols[i]){
+            const gameSpace = document.createElement("button");
+            gameSpace.setAttribute("id",nameSpace);
+            gameSpace.classList.add("space");
+            gameSpace.textContent =""
+            gameBoard.appendChild(gameSpace);
+        }
+    };
+
+
+    
+    gameContainer.appendChild(newPlayers);
+    gameContainer.appendChild(scoreBoard);
+    gameContainer.appendChild(gameBoard);
+    document.body.appendChild(gameContainer);
+    
+}
+
 
 getPlayers();
 
